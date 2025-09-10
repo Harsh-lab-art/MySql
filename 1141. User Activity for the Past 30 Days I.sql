97c9065638ea26a6ae72733655b6538f45171878
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-09-10 22:40:58 +0530

    1141. User Activity for the Past 30 Days I.sql

---
+++ 1141. User Activity for the Past 30 Days I.sql
@@ -0,0 +1,7 @@
+select
+    activity_date as day,
+    COUNT(distinct  user_id) as active_users
+from Activity
+where activity_date between DATE_SUB('2019-07-27', INTERVAL 29 DAY)and '2019-07-27'
+group by activity_date;
+
